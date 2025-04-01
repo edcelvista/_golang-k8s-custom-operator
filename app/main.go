c9dd4b55e9a3095daeedf3c6a9e41cc30ba9a6e8
Edcel Vista <administrator@edcelvista.com>
2025-04-01 21:27:19 +0800

    operator go refactor receiver functions

--- app/main.go
+++ app/main.go
@@ -33,54 +33,54 @@ var APPNAME string = "demo-custom-resource"
 var INTERVAL int = 15
 var K8S_TIMEOUT int32 = 60
 
-func checkRequiredEnv() {
+func checkRequiredEnv() error {
 	targetNs := os.Getenv("TARGETNAMESPACE")
 	if targetNs != "" {
 		TARGETNAMESPACE = targetNs
-		log.Println("Env Config in found", TARGETNAMESPACE)
+		log.Println("💡 Env Config in found", TARGETNAMESPACE)
 	}
 
 	appName := os.Getenv("APPNAME")
 	if appName != "" {
 		APPNAME = appName
-		log.Println("Env Config in found", APPNAME)
+		log.Println("💡 Env Config in found", APPNAME)
 	}
 
 	crdTarget := os.Getenv("CRDNAME")
 	if crdTarget != "" {
 		CRDNAME = crdTarget
-		log.Println("Env Config in found", CRDNAME)
+		log.Println("💡 Env Config in found", CRDNAME)
 	}
 
 	groupTarget := os.Getenv("CRDGROUP")
 	if groupTarget != "" {
 		CRDGROUP = groupTarget
-		log.Println("Env Config in found", CRDGROUP)
+		log.Println("💡 Env Config in found", CRDGROUP)
 	}
 
 	verTarget := os.Getenv("CRDVERSION")
 	if verTarget != "" {
 		CRDVERSION = verTarget
-		log.Println("Env Config in found", CRDVERSION)
+		log.Println("💡 Env Config in found", CRDVERSION)
 	}
 
 	resourceTarget := os.Getenv("CRDRESOURCE")
 	if resourceTarget != "" {
 		CRDRESOURCE = resourceTarget
-		log.Println("Env Config in found", CRDRESOURCE)
+		log.Println("💡 Env Config in found", CRDRESOURCE)
 	}
 
 	interval := os.Getenv("INTERVAL")
 	if interval != "" {
 		num, err := strconv.Atoi(interval)
 		if err != nil {
-			fmt.Println("Error:", err)
-			return
+			fmt.Println("‼️ Error:", err)
+			return err
 		}
 		INTERVAL = num
-		log.Println("Env Interval in found", INTERVAL)
+		log.Println("💡 Env Interval in found", INTERVAL)
 	}
-
+	return nil
 }
 
 func checkKubeConfig() bool {
@@ -89,20 +89,21 @@ func checkKubeConfig() bool {
 		KUBECONFIG = customKubeConfig
 		_, err := os.Stat(KUBECONFIG)
 		if !os.IsNotExist(err) {
-			log.Println("Config in found [CUSTOM_KUBE_CONFIG_PATH]", KUBECONFIG)
+			log.Println("💡 ⚡️ Config in found [CUSTOM_KUBE_CONFIG_PATH]", KUBECONFIG)
 			return true
 		}
 	}
 
 	defaultHome := os.ExpandEnv("$HOME")
-	_, err2 := os.Stat(defaultHome)
-	if !os.IsNotExist(err2) {
-		log.Println("Config in found [Default Home]", defaultHome)
-		KUBECONFIG = fmt.Sprintf("%v/.kube/config", defaultHome)
+	homeKubeConfig := fmt.Sprintf("%s/.kube/config", defaultHome)
+	_, err := os.Stat(homeKubeConfig)
+	if !os.IsNotExist(err) {
+		log.Println("💡 ⚡️ Config in found [Default Home]", homeKubeConfig)
+		KUBECONFIG = homeKubeConfig
 		return true
 	}
 
-	log.Println("Config in not found", KUBECONFIG, defaultHome)
+	log.Println("‼️ Config in not found", KUBECONFIG, homeKubeConfig)
 	return false
 }
 
@@ -110,32 +111,32 @@ func initConnection() (*kubernetes.Clientset, *apiextensionsclientset.Clientset,
 	// Load kubeconfig file (for running locally)
 	config, err := clientcmd.BuildConfigFromFlags("", KUBECONFIG)
 	if err != nil {
-		log.Fatalf("Error loading kubeconfig: %v", err)
+		log.Fatalf("‼️ Error loading kubeconfig: %v", err)
 	}
 
 	// Create clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		log.Fatalf("Error creating Kubernetes client: %v", err)
+		log.Fatalf("‼️ Error creating Kubernetes client: %v", err)
 	}
 
 	// Create clientset api ext
 	clientsetExt, err := apiextensionsclientset.NewForConfig(config)
 	if err != nil {
-		log.Fatalf("Error creating Kubernetes client: %v", err)
+		log.Fatalf("‼️ Error creating Kubernetes client: %v", err)
 	}
 
 	// Create clientset dynamic client
 	clientSetDynamic, err := dynamic.NewForConfig(config)
 	if err != nil {
-		log.Fatalf("Error creating Kubernetes client: %v", err)
+		log.Fatalf("‼️ Error creating Kubernetes client: %v", err)
 	}
 
 	version, err := clientset.Discovery().ServerVersion()
 	if err != nil {
-		log.Fatalf("Error retrieving server version: %v", err)
+		log.Fatalf("‼️ Error retrieving server version: %v", err)
 	}
-	log.Println("Cluster Version:", version.String())
+	log.Println("💡 ⚡️ Cluster Version:", version.String())
 
 	return clientset, clientsetExt, clientSetDynamic
 }
@@ -145,23 +146,28 @@ func main() {
 	ctx := context.Background()
 
 	// ENV Init
-	checkRequiredEnv()
+	err := checkRequiredEnv()
+	if err != nil {
+		log.Fatalf("‼️ Failed to initialize Environment Variables: %v", err)
+	}
+
 	isConfigExist := checkKubeConfig()
+	if !isConfigExist {
+		log.Fatalf("‼️ Failed to find the kube/config. isConfigExist: %v", isConfigExist)
+	}
 	log.Println("IsKubeConfigExist:", strconv.FormatBool(isConfigExist))
 
 	// Init Connections
 	cs, cse, csed := initConnection()
 
 	// Store the object i.e Connections Obj in the context using context.WithValue
-	ctx = context.WithValue(ctx, "clientSet", cs)
-	ctx = context.WithValue(ctx, "clientSetExt", cse)
-	ctx = context.WithValue(ctx, "clientSetExtDynamic", csed)
-	ctx = context.WithValue(ctx, "nodeListOptions", metav1.ListOptions{})
-	ctx = context.WithValue(ctx, "crdListOptions", metav1.ListOptions{
-		FieldSelector: fmt.Sprintf("metadata.name=%s", CRDNAME),
-	})
-	ctx = context.WithValue(ctx, "crdResourceListOptions", metav1.ListOptions{})
-	ctx = context.WithValue(ctx, "crdResourceGetOptions", metav1.GetOptions{})
+	ctx = context.WithValue(ctx, clientSet, cs)
+	ctx = context.WithValue(ctx, clientSetExt, cse)
+	ctx = context.WithValue(ctx, clientSetExtDynamic, csed)
+	ctx = context.WithValue(ctx, crdListOptions, metav1.ListOptions{})
+	ctx = context.WithValue(ctx, crdListOptions, metav1.ListOptions{FieldSelector: fmt.Sprintf("metadata.name=%s", CRDNAME)})
+	ctx = context.WithValue(ctx, crdResourceListOptions, metav1.ListOptions{})
+	ctx = context.WithValue(ctx, crdResourceGetOptions, metav1.GetOptions{})
 	// ctx = context.WithValue(ctx, "podListOptions", metav1.ListOptions{})
 
 	// Check Functions
@@ -170,124 +176,124 @@ func main() {
 
 	// Check CRD Resource
 	tick := time.Tick(time.Duration(INTERVAL) * time.Second)
+	operator := Operator{
+		ctx: ctx,
+		gvr: schema.GroupVersionResource{ // Define the GVR (GroupVersionResource)
+			Group:    CRDGROUP,
+			Version:  CRDVERSION,
+			Resource: CRDRESOURCE,
+		},
+	}
+
 	for range tick {
-		runOperator(ctx)
+		log.Printf("🪝 Ticking...")
+		operator.Run()
 	}
 }
 
 func checkNodes(ctx context.Context) {
-	clientSet := ctx.Value("clientSet").(*kubernetes.Clientset)
-	nodeListOptions := ctx.Value("nodeListOptions").(metav1.ListOptions)
+	clientSet := ctx.Value(clientSet).(*kubernetes.Clientset)
+	nodeListOptions := ctx.Value(crdListOptions).(metav1.ListOptions)
 
 	nodes, err := clientSet.CoreV1().Nodes().List(ctx, nodeListOptions)
 	if err != nil {
-		log.Fatalf("Error listing pods: %v\n", err)
+		log.Fatalf("‼️ Error listing nodes: %v\n", err)
 	}
 
-	log.Printf("NODES: %v", len(nodes.Items))
+	log.Printf("🖥️  NODES: %v", len(nodes.Items))
 	for _, v := range nodes.Items {
-		log.Println(" >", v.Name, v.Status.NodeInfo.KubeletVersion, v.Status.NodeInfo.Architecture, v.Status.NodeInfo.MachineID, v.Status.Conditions[4])
+		log.Println("⚡️ ", v.Name, v.Status.NodeInfo.KubeletVersion, v.Status.NodeInfo.Architecture, v.Status.NodeInfo.MachineID, v.Status.Conditions[4])
 	}
 }
 
 func checkCrds(ctx context.Context) {
-	clientSet := ctx.Value("clientSetExt").(*apiextensionsclientset.Clientset)
-	crdListOptions := ctx.Value("crdListOptions").(metav1.ListOptions)
+	clientSet := ctx.Value(clientSetExt).(*apiextensionsclientset.Clientset)
+	crdListOptions := ctx.Value(crdListOptions).(metav1.ListOptions)
 
 	crds, err := clientSet.ApiextensionsV1().CustomResourceDefinitions().List(ctx, crdListOptions)
 	if err != nil {
-		log.Fatalf("Error listing CRDs: %v", err)
+		log.Fatalf("‼️ Error listing CRDs: %v", err)
 	}
 
-	log.Printf("CRD: %v\n", len(crds.Items))
+	log.Printf("📝 CRD: %v\n", len(crds.Items))
 	for _, v := range crds.Items {
-		log.Println(" >", v.Name)
+		log.Println("⚡️", v.Name)
 	}
 }
 
-func runOperator(ctx context.Context) {
-	// Define the GVR (GroupVersionResource)
-	gvr := schema.GroupVersionResource{
-		Group:    CRDGROUP,
-		Version:  CRDVERSION,
-		Resource: CRDRESOURCE,
+func (m *Operator) Run() {
+	cr := CRDResources{
+		ctx: m.ctx,
+		gvr: m.gvr,
 	}
-
-	crdResources := checkCRDResources(ctx, gvr)
+	crdResources := cr.get()
 	for i := 0; i < len(crdResources); i++ {
 		crdResourcePayload := crdResources[i]
-		splitted := strings.Split(crdResourcePayload, "|") // namespace|name
-		resourceDetail := getCRDResourceDetails(ctx, gvr, splitted[0], splitted[1])
-		resourceDetail.reconcile(ctx)
+		splitted := strings.Split(crdResourcePayload, "|") // name|namespace
+		crd := CRDResourceDetails{
+			ctx:       m.ctx,
+			gvr:       m.gvr,
+			name:      splitted[0],
+			namespace: splitted[1],
+		}
+		resourceDetail := crd.get()
+		resourceDetail.reconcile(m.ctx)
+	}
+
+	if len(crdResources) == 0 {
+		log.Println("⚠️ No CRD Found")
 	}
 }
 
-// func checkPods(ctx context.Context, listOptions metav1.ListOptions) *v1.PodList {
-// 	clientSet := ctx.Value("clientSet").(*kubernetes.Clientset)
-// 	podListOptions := ctx.Value("podListOptions").(metav1.ListOptions)
+// func (m *GetPods) checkPods() *v1.PodList {
+// 	clientSet := m.ctx.Value(clientSet).(*kubernetes.Clientset)
+// 	podListOptions := m.ctx.Value("podListOptions").(metav1.ListOptions)
 
-// 	pods, err := clientSet.CoreV1().Pods(TARGETNAMESPACE).List(ctx, podListOptions)
+// 	pods, err := clientSet.CoreV1().Pods(TARGETNAMESPACE).List(m.ctx, podListOptions)
 // 	if err != nil {
-// 		log.Fatalf("Error listing pods: %v", err)
+// 		log.Fatalf("‼️ Error listing pods: %v", err)
 // 	}
 
-// 	log.Printf("PODS: %v\n", len(pods.Items))
+// 	log.Printf("⚙️ PODS: %v\n", len(pods.Items))
 // 	for _, v := range pods.Items {
 // 		for _, c := range v.Status.ContainerStatuses {
-// 			log.Printf(" > %v/%v container %v is ready: (%v)\n", v.Namespace, v.Name, c.Name, c.Ready)
+// 			log.Printf(" ⚡️ %v/%v container %v is ready: (%v)\n", v.Namespace, v.Name, c.Name, c.Ready)
 // 		}
 // 	}
 
 // 	return pods
 // }
 
-func checkDeployment(ctx context.Context, listOptions metav1.ListOptions) *appsv1.DeploymentList {
-	clientSet := ctx.Value("clientSet").(*kubernetes.Clientset)
+func (m *CRDResources) get() []string {
+	clientSet := m.ctx.Value(clientSetExtDynamic).(*dynamic.DynamicClient)
+	crdResourceListOptions := m.ctx.Value(crdResourceListOptions).(metav1.ListOptions)
 
-	deployments, err := clientSet.AppsV1().Deployments(TARGETNAMESPACE).List(ctx, listOptions)
+	crdResources, err := clientSet.Resource(m.gvr).Namespace(TARGETNAMESPACE).List(m.ctx, crdResourceListOptions)
 	if err != nil {
-		log.Fatalf("Error listing Deployment: %v", err)
+		log.Fatalf("‼️ Error listing CRD Resource: %v", err)
 	}
 
-	log.Printf("DEPLOYMENT: %v\n", len(deployments.Items))
-	for _, v := range deployments.Items {
-		log.Printf(" > %v/%v", v.Namespace, v.Name)
-		log.Printf("PODS: Replicas (%v) | Available (%v) | Ready (%v) | Unavailable (%v)\n", v.Status.Replicas, v.Status.AvailableReplicas, v.Status.ReadyReplicas, v.Status.UnavailableReplicas)
-	}
-
-	return deployments
-}
-
-func checkCRDResources(ctx context.Context, gvr schema.GroupVersionResource) []string {
-	clientSet := ctx.Value("clientSetExtDynamic").(*dynamic.DynamicClient)
-	crdResourceListOptions := ctx.Value("crdResourceListOptions").(metav1.ListOptions)
-
-	crdResources, err := clientSet.Resource(gvr).Namespace(TARGETNAMESPACE).List(ctx, crdResourceListOptions)
-	if err != nil {
-		log.Fatalf("Error listing CRD Resource: %v", err)
-	}
-
-	log.Printf("CRD RESOURCE: %v \n", len(crdResources.Items))
+	log.Printf("🗒️ CRD RESOURCE: %v \n", len(crdResources.Items))
 	resourceNames := []string{}
 	for _, v := range crdResources.Items {
 		resourceCollation := fmt.Sprintf("%v|%v", v.GetName(), v.GetNamespace())
 		resourceNames = append(resourceNames, resourceCollation)
-		log.Printf(" > %v/%v", v.GetNamespace(), v.GetName())
+		log.Printf("⚡️ %v/%v", v.GetNamespace(), v.GetName())
 	}
 
 	return resourceNames
 }
 
-func getCRDResourceDetails(ctx context.Context, gvr schema.GroupVersionResource, name string, namespace string) MyApp {
-	clientSet := ctx.Value("clientSetExtDynamic").(*dynamic.DynamicClient)
-	crdResourceGetOptions := ctx.Value("crdResourceGetOptions").(metav1.GetOptions)
+func (m *CRDResourceDetails) get() *MyApp {
+	clientSet := m.ctx.Value(clientSetExtDynamic).(*dynamic.DynamicClient)
+	crdResourceGetOptions := m.ctx.Value(crdResourceGetOptions).(metav1.GetOptions)
 
-	crdResourceObj, err := clientSet.Resource(gvr).Namespace(namespace).Get(ctx, name, crdResourceGetOptions)
+	crdResourceObj, err := clientSet.Resource(m.gvr).Namespace(m.namespace).Get(m.ctx, m.name, crdResourceGetOptions)
 	if err != nil {
-		log.Fatalf("Error listing CRD Resource: %v", err)
+		log.Fatalf("‼️ Error listing CRD Resource: %v", err)
 	}
 
-	// Manual mapping of unstructured object
+	// Manual mapping of unstructured object, assert type and build struct data
 	metadata := crdResourceObj.Object["metadata"].(map[string]interface{})
 	spec := crdResourceObj.Object["spec"].(map[string]interface{})
 	myApp := MyApp{
@@ -302,7 +308,24 @@ func getCRDResourceDetails(ctx context.Context, gvr schema.GroupVersionResource,
 		},
 	}
 
-	return myApp
+	return &myApp
+}
+
+func (m *Deployments) get() *appsv1.DeploymentList {
+	clientSet := m.ctx.Value(clientSet).(*kubernetes.Clientset)
+
+	deployments, err := clientSet.AppsV1().Deployments(TARGETNAMESPACE).List(m.ctx, m.listOptions)
+	if err != nil {
+		log.Fatalf("‼️ Error listing Deployment: %v", err)
+	}
+
+	log.Printf("⚙️ DEPLOYMENT: %v\n", len(deployments.Items))
+	for _, v := range deployments.Items {
+		log.Printf("⚡️ %v/%v", v.Namespace, v.Name)
+		log.Printf("🛠️ PODS: Replicas (%v) | Available (%v) | Ready (%v) | Unavailable (%v)\n", v.Status.Replicas, v.Status.AvailableReplicas, v.Status.ReadyReplicas, v.Status.UnavailableReplicas)
+	}
+
+	return deployments
 }
 
 func (m *MyApp) reconcile(ctx context.Context) {
@@ -312,11 +335,15 @@ func (m *MyApp) reconcile(ctx context.Context) {
 	}
 
 	// Check Pods & Deployments
-	deployments := checkDeployment(ctx, listOptions)
+	deps := Deployments{
+		ctx:         ctx,
+		listOptions: listOptions,
+	}
+	deployments := deps.get()
 	// pods := checkPods(ctx, listOptions)
 
 	if len(deployments.Items) == 0 {
-		log.Printf("Reconciling Creating Deployment...")
+		log.Printf("⚠️ ⚡️ Reconciling Creating Deployment...")
 		m.createDeployment(ctx)
 		return
 	}
@@ -324,26 +351,36 @@ func (m *MyApp) reconcile(ctx context.Context) {
 	isChanged := false
 	for _, v := range deployments.Items {
 		if m.spec.replicas != int64(v.Status.Replicas) {
-			log.Printf("Reconciling Scaling Deployment: %v/%v from %v => %v", v.Namespace, v.Name, v.Status.Replicas, m.spec.replicas)
-			scaleDeployment(ctx, m.spec.replicas, v.Name)
+			log.Printf("⚠️ ⚡️ Reconciling Scaling Deployment: %v/%v from %v => %v", v.Namespace, v.Name, v.Status.Replicas, m.spec.replicas)
+			deploymentForScale := ScaleDeploymentParams{
+				ctx:      ctx,
+				replicas: m.spec.replicas,
+				name:     v.Name,
+			}
+			deploymentForScale.scaleDeployment()
 			isChanged = true
 		}
 
 		if m.spec.image != v.Spec.Template.Spec.Containers[0].Image {
-			log.Printf("Reconciling Patching Deployment: %v/%v from %v => %v", v.Namespace, v.Name, v.Spec.Template.Spec.Containers[0].Image, m.spec.image)
+			log.Printf("⚠️ ⚡️ Reconciling Patching Deployment: %v/%v from %v => %v", v.Namespace, v.Name, v.Spec.Template.Spec.Containers[0].Image, m.spec.image)
 			patch := fmt.Sprintf(`[{ "op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "%v" }]`, m.spec.image)
-			updateDeployment(ctx, v.Name, patch)
+			deploymentForUpdate := UpdateDeploymentParams{
+				ctx:       ctx,
+				name:      v.Name,
+				patchJson: patch,
+			}
+			deploymentForUpdate.updateDeployment()
 			isChanged = true
 		}
 	}
 
 	if !isChanged {
-		log.Printf("CRD DESIRED (%v)... NO ACTION NEEDED...", m.spec.replicas)
+		log.Printf("✅ CRD DESIRED (%v)... NO ACTION NEEDED...", m.spec.replicas)
 	}
 }
 
 func (m *MyApp) createDeployment(ctx context.Context) {
-	clientSet := ctx.Value("clientSet").(*kubernetes.Clientset)
+	clientSet := ctx.Value(clientSet).(*kubernetes.Clientset)
 
 	// Define the Deployment
 	deployment := &appsv1.Deployment{
@@ -376,58 +413,65 @@ func (m *MyApp) createDeployment(ctx context.Context) {
 
 	// Create the Deployment
 	deploymentsClient := clientSet.AppsV1().Deployments(TARGETNAMESPACE)
-	ctxDep, cancel := context.WithTimeout(context.Background(), time.Duration(K8S_TIMEOUT)*time.Second)
+	ctxDep, cancel := context.WithTimeout(ctx, time.Duration(K8S_TIMEOUT)*time.Second)
 	defer cancel()
 
 	result, err := deploymentsClient.Create(ctxDep, deployment, metav1.CreateOptions{})
 	if err != nil {
-		log.Fatalf("Failed to create deployment: %v", err)
+		log.Fatalf("‼️ Failed to create deployment: %v", err)
 	}
 
-	log.Printf("Deployment %q created successfully\n", result.GetObjectMeta().GetName())
+	log.Printf("⚙️ Deployment %q created successfully...\n", result.GetObjectMeta().GetName())
 }
 
-func updateDeployment(ctx context.Context, deploymentName string, patchJson string) {
-	clientSet := ctx.Value("clientSet").(*kubernetes.Clientset)
+func (m *UpdateDeploymentParams) updateDeployment() error {
+	clientSet := m.ctx.Value(clientSet).(*kubernetes.Clientset)
 
 	// Create the Deployment
 	deploymentsClient := clientSet.AppsV1().Deployments(TARGETNAMESPACE)
-	ctxDep, cancel := context.WithTimeout(context.Background(), time.Duration(K8S_TIMEOUT)*time.Second)
+	ctxDep, cancel := context.WithTimeout(m.ctx, time.Duration(K8S_TIMEOUT)*time.Second)
 	defer cancel()
 
-	patch := []byte(patchJson)
+	patch := []byte(m.patchJson)
 
-	_, err := deploymentsClient.Patch(ctxDep, deploymentName, types.JSONPatchType, patch, metav1.PatchOptions{})
+	_, err := deploymentsClient.Patch(ctxDep, m.name, types.JSONPatchType, patch, metav1.PatchOptions{})
 	if err != nil {
-		log.Fatalf("Failed to apply patch: %v", err)
+		log.Fatalf("‼️ Failed to apply patch: %v", err)
+		return err
 	}
 
-	log.Printf("Deployment %s patched with %v\n", deploymentName, patchJson)
+	log.Printf("⚙️ Deployment %s patched with %v\n", m.name, m.patchJson)
+
+	return nil
 }
 
-func scaleDeployment(ctx context.Context, replicas int64, deploymentName string) {
-	clientSet := ctx.Value("clientSet").(*kubernetes.Clientset)
+func (m *ScaleDeploymentParams) scaleDeployment() error {
+	clientSet := m.ctx.Value(clientSet).(*kubernetes.Clientset)
 
 	// Create the Deployment
 	deploymentsClient := clientSet.AppsV1().Deployments(TARGETNAMESPACE)
-	ctxDep, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctxDep, cancel := context.WithTimeout(m.ctx, 10*time.Second)
 	defer cancel()
 
 	// Get current scale
-	scale, err := clientSet.AppsV1().Deployments(TARGETNAMESPACE).GetScale(context.TODO(), deploymentName, metav1.GetOptions{})
+	scale, err := clientSet.AppsV1().Deployments(TARGETNAMESPACE).GetScale(ctxDep, m.name, metav1.GetOptions{})
 	if err != nil {
-		log.Fatalf("Failed to get scale: %v", err)
+		log.Fatalf("‼️ Failed to get scale: %v", err)
+		return err
 	}
 
 	// Update the number of replicas
-	scale.Spec.Replicas = int64ToInt32Ptr(replicas) // New replica count
+	scale.Spec.Replicas = int64ToInt32Ptr(m.replicas) // New replica count
 
-	_, err2 := deploymentsClient.UpdateScale(ctxDep, deploymentName, scale, metav1.UpdateOptions{
+	_, err = deploymentsClient.UpdateScale(ctxDep, m.name, scale, metav1.UpdateOptions{
 		FieldManager: "scale-controller",
 	})
-	if err2 != nil {
-		log.Fatalf("Failed to apply scale: %v", err2)
+	if err != nil {
+		log.Fatalf("‼️ Failed to apply scale: %v", err)
+		return err
 	}
 
-	log.Printf("Deployment %s scaled to %d replicas\n", deploymentName, replicas)
+	log.Printf("⚙️ Deployment %s scaled to %d replicas\n", m.name, m.replicas)
+
+	return nil
 }
--- app/types.go
+++ app/types.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"context"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+type ctxKeyClientSet string
+type ctxKeyClientSetExt string
+type ctxKeyClientSetExtDynamic string
+type ctxKeyCodeListOptions string
+type ctxKeyCrdListOptions string
+type ctxKeyCrdResourceListOptions string
+type ctxKeyCrdResourceGetOptions string
+
+const (
+	clientSet              ctxKeyClientSet              = "ctxKeyClientSet"
+	clientSetExt           ctxKeyClientSetExt           = "ctxKeyClientSetExt"
+	clientSetExtDynamic    ctxKeyClientSetExtDynamic    = "ctxKeyClientSetExtDynamic"
+	codeListOptions        ctxKeyCodeListOptions        = "ctxKeyCodeListOptions"
+	crdListOptions         ctxKeyCrdListOptions         = "ctxKeyCrdListOptions"
+	crdResourceListOptions ctxKeyCrdResourceListOptions = "ctxKeyCrdResourceListOptions"
+	crdResourceGetOptions  ctxKeyCrdResourceGetOptions  = "ctxKeyCrdResourceGetOptions"
+)
+
 type MyApp struct {
 	metadata MyAppMetadata
 	spec     MyAppSpec
@@ -14,4 +39,43 @@ type MyAppSpec struct {
 	image       string
 	replicas    int64
 	appSelector string
+}
+
+// type GetPods struct {
+// 	ctx         context.Context
+// 	listOptions metav1.ListOptions
+// }
+
+type Deployments struct {
+	ctx         context.Context
+	listOptions metav1.ListOptions
+}
+
+type UpdateDeploymentParams struct {
+	ctx       context.Context
+	name      string
+	patchJson string
+}
+
+type ScaleDeploymentParams struct {
+	ctx      context.Context
+	replicas int64
+	name     string
+}
+
+type Operator struct {
+	ctx context.Context
+	gvr schema.GroupVersionResource
+}
+
+type CRDResources struct {
+	ctx context.Context
+	gvr schema.GroupVersionResource
+}
+
+type CRDResourceDetails struct {
+	ctx       context.Context
+	gvr       schema.GroupVersionResource
+	name      string
+	namespace string
 }

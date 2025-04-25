431f5225dba3523da20bd295958c853a8caaa40c
Edcel Vista <administrator@edcelvista.com>
2025-04-25 21:47:05 +0800

    sans based ssl

--- webhook-admission-controller/_generate-ssl.sh
+++ webhook-admission-controller/_generate-ssl.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=*.custom-webhook.svc/O=edcelvistadotcom"
+# openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=*.custom-webhook.svc/O=edcelvistadotcom"
+openssl req -new -newkey rsa:2048 -days 365 -nodes -keyout tls.key -out tls.csr -config openssl.cnf
+openssl x509 -req -in tls.csr -signkey tls.key -out tls.crt -extensions v3_req -extfile openssl.cnf

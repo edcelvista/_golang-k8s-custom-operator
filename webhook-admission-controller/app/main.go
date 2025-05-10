e8880dfad1f4f9289751bc837047a59983fd5c4a
Edcel Vista <administrator@edcelvista.com>
2025-05-10 21:57:23 +0800

    yml manipulation

--- webhook-admission-controller/app/main.go
+++ webhook-admission-controller/app/main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	Router "_gorestapi-k8s/router"
+	"log"
+	"net/http"
+	_ "net/http/pprof" // automatically expose /debug/pprof/
+	"os"
+	// CPU Profiling: /debug/pprof/profile?seconds=30 (takes a 30-second snapshot)
+	// Goroutines: /debug/pprof/goroutine
+	// Heap (memory): /debug/pprof/heap
+	// Block: /debug/pprof/block
+	// OR
+	// go tool pprof -http=localhost:6061 "http://localhost:6060/debug/pprof/profile?seconds=30"
+	// Requires brew install graphviz
 )
 
 func main() {
+	if os.Getenv("APP_ENV") == "development" {
+		log.Println("Enabling pprof for profiling")
+		go func() {
+			log.Println(http.ListenAndServe("localhost:6060", nil))
+		}()
+	}
+
 	Router.Run()
 }
---
+++ yaml-manipulation/patch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+INPUT="input.yml"
+
+# Store modified YAML to a variable
+MODIFIED_YAML_RQ=$(yq eval '
+.container_config |= map(
+    .resource_quota = {
+        "requests.cpu": "1",
+        "requests.memory": "1Gi",
+        "limits.cpu": "2",
+        "limits.memory": "2Gi",
+        "pods": "2",
+        "persistentvolumeclaims": "5",
+        "requests.storage": "5Gi"
+    }
+)
+' "$INPUT")
+
+MODIFIED_YAML=$(yq eval '
+.container_config[] |= (
+  .namespaces = ["namespace1", "namespace2"]
+)
+' <<< "$MODIFIED_YAML_RQ")
+
+# Print or use
+echo "$MODIFIED_YAML" > output.yaml
